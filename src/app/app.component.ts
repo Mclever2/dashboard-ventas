474beda62118f46246a18d62479212d2dc52415e
Mclever2 <166152186+Mclever2@users.noreply.github.com>
2026-01-12 23:48:59 -0500

    firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   isCollapsed = false;
   constructor(private firestore: AngularFirestore) {
     this.firestore.collection('test').add({ hola: 'mundo', fecha: new Date() })
-      .then(() => console.log('✅ Conexión a Firebase EXITOSA!'))
-      .catch(err => console.error('❌ Error de conexión:', err));
+      .then(() => console.log('Conexión a Firebase EXITOSA!'))
+      .catch(err => console.error('Error de conexión:', err));
   }
 }
